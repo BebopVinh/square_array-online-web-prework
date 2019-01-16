e6dd071713409a9b9fed3900f8a6c2fda92bbf35
Vinh Do <vinhdo07@gmail.com>
2019-01-16 02:34:50 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,6 +2,6 @@ def square_array(array)
   counter = 0
   new_square_array = []
   while counter < array.size
-    new_square_array = new_square_array.push(array[counter]*array[counter])
+    new_square_array = new_square_array.push(array[counter]**2)
   end
 end
